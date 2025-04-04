ffe21f2e27a541d249e2ad67ce5a934dac951f9e
gurbanovcavad <gurbanovcavad7@gmail.com>
2025-04-04 16:44:32 +0400

    New Feature Add

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -18,6 +18,7 @@ def save_entry(title, content):
 
 
 def get_entry(title):
+    print("function: " + title)
     try:
         f = default_storage.open(f"entries/{title}.md")
         return f.read().decode("utf-8")
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
-
+    arr = util.list_entries()
+    newArr = []
+    if 'q' in request.GET:
+        q = request.GET['q'] 
+        if q in arr:
+            return HttpResponseRedirect(reverse('open', args=[q]))
+        else:
+            for i in arr:
+                if q in i:
+                    newArr.append(i)
+    else:
+        newArr = arr
+    context = {'entries': newArr}
+    return render(request, "encyclopedia/index.html", context)
+    
+def open(request, title):
+    x = util.get_entry(title)
+    if not x:
+        response = render(request, 'encyclopedia/404.html')
+        response.status_code = 404
+        return response
+    context = {'name': title, 'content': x}
+    return render(request, 'encyclopedia/open.html', context)
